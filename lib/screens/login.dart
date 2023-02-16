3cd5ee508293286359e1855d0fab817bb3bb23ab
Kseniya <ksilina39@gmail.com>
2023-02-16 16:46:19 +0200

    commit

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_stripe/flutter_stripe.dart';
-//import 'package:http/http.dart' as http;
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:developer';
 
 import '../data/firebase_helper.dart';
+
+const url = "https://us-central1-example-ed4df.cloudfunctions.net/stripePaymentIntentRequest";
 
 class LoginScreen extends StatelessWidget {
   final _emailController = TextEditingController();
@@ -96,7 +100,7 @@ class LoginScreen extends StatelessWidget {
                       MaterialStateProperty.all<Color>(Colors.blue),
                 ),
                 
-                onPressed: () {},// тут чет надо
+                 onPressed: () => initPaymentSheet(context, email: "example@gmail.com", amount: 200000),
                 
                 child: const Text(
                   'Pay',
@@ -115,4 +119,51 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
+ Future<void> initPaymentSheet(context, {required String email, required int amount}) async {
+    try {
+      // 1. create payment intent on the server
+      final response = await http.post(
+          Uri.parse(
+            url,
+          ),
+          body: {
+            'email': email,
+            'amount': amount.toString(),
+          });
+
+      final jsonResponse = jsonDecode(response.body);
+      log(jsonResponse.toString());
+
+      //2. initialize the payment sheet
+      await Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+          paymentIntentClientSecret: jsonResponse['paymentIntent'],
+          merchantDisplayName: 'Flutter Stripe Store Demo',
+          customerId: jsonResponse['customer'],
+          customerEphemeralKeySecret: jsonResponse['ephemeralKey'],
+          style: ThemeMode.light,
+        ),
+      );
+
+      await Stripe.instance.presentPaymentSheet();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Payment completed!')),
+      );
+    } catch (e) {
+			log(e.toString());
+      if (e is StripeException) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error from Stripe: ${e.error.localizedMessage}'),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
+    }
+  }
+
 }
